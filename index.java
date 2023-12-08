408b5ee8f5c5d0c709ac85b6e71fcfbf9ca92e46
1vanskiy <1vanskiyyyyy@gmail.com>
2023-12-08 09:54:38 +0200

    first commit from cmd?

---
+++ index.java
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+
+        <header class="header">
+            <div class="logo"></div>
+            <div class="navigation"></div>
+        </header>
+        <main class="main"></main>
+        <footer class="footer"></footer>
+    </div>
+</body>
+</html>
